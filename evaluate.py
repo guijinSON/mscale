ec550f41386519dcbd06710086fe79b636a85d34
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2024-07-23 15:06:59 +0900

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,8 @@
 import argparse
 from vllm import LLM, SamplingParams
 from logits import ban_illegal_tokens, get_allowed_token_ids
-from src.template import kmmlu_mcqa,BLEND_en_mcqa
-from src.data_loader import load_kmmlu_data, load_blend_data
+from src.template import kmmlu_mcqa,BLEND_en_mcqa, indommlu_mcqa
+from src.data_loader import load_kmmlu_data, load_blend_data, load_indommlu_data
 import pandas as pd
 
 def parse_arguments():
@@ -17,14 +17,21 @@ def parse_arguments():
 
 
 
-def prepare_queries(df,template):
-    try:
+def prepare_queries(df,template,dataset_name):
+    if dataset_name is in ['kmmlu']:
         return [{'query': template.format(row.question, row.A, row.B, row.C, row.D),
                 'answer': ['A', 'B', 'C', 'D'][row.answer-1],
                 'category': row.Category} for _, row in df.iterrows()]
-    except:
+    
+    elif dataset_name is in ['blend']:
         return [{'query': template.format(row.question, row.A, row.B, row.C, row.D),
                 'answer': ['A', 'B', 'C', 'D'][row.answer-1]} for _, row in df.iterrows()]
+    
+    elif dataset_name is in ['indommlu']:
+        return [{'query': template.format(row.question, *eval(row.options)),
+                  'answer' : row.answer,
+                  'category':row.subject} for _,row in df.iterrows()]
+                    
 
 def evaluate_model(model, queries, sampling_params):
     outputs = model.generate([item['query'] for item in queries], sampling_params)
@@ -37,12 +44,16 @@ def main():
 
     if args.dataset == "kmmlu":
         df = load_kmmlu_data()
-        queries = prepare_queries(df,kmmlu_mcqa)
+        queries = prepare_queries(df,kmmlu_mcqa,args.dataset)
+        
     elif args.dataset == "indommlu":
-        print("IndoMMlu dataset not yet implemented")
+        df = load_indommlu_data()
+        queries = prepare_queries(df,indommlu_mcqa,args.dataset)
+        
     elif args.dataset == "blend":
         df = load_blend_data(args.lan)
-        queries = prepare_queries(df,BLEND_en_mcqa)
+        queries = prepare_queries(df,BLEND_en_mcqa,args.dataset)
+        
     model = LLM(model=args.model)
     allowed_token_ids = get_allowed_token_ids(model, args.allowed_tokens)
     sampling_params = SamplingParams(
