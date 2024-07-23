d76440ebed6f1ce4ca084e8034a71429a9e6edef
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2024-07-23 15:59:20 +0900

    Update template.py

--- src/template.py
+++ src/template.py
@@ -1,3 +1,36 @@
+belebele_mcqa_ko = """주어진 문제와 자료를 읽고 정답을 가장 알맞은 것을 고르시요.
+### 질문: {}
+### 자료:
+{}
+### 선택지:
+    A. {}
+    B. {}
+    C. {}
+    D. {}
+### 정답:"""
+
+belebele_mcqa_en ="""Read the given question and passage and choose the most appropriate answer.
+### Question: {}
+### Passage:
+{}
+### Options:
+    A. {}
+    B. {}
+    C. {}
+    D. {}
+### Answer:"""
+
+belebele_mcqa_indo = """Bacalah soal dan materi yang diberikan dan pilihlah jawaban yang paling tepat.
+### Pertanyaan: {}
+### Materi:
+{}
+### Pilihan:
+    A. {}
+    B. {}
+    C. {}
+    D. {}
+### Jawaban:"""
+
 mmlu_mcqa = """Read the following question and choose the most appropriate answer.
 ### Question: {}
 ### Options:
